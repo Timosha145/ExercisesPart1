93f441ffd9d16c0bc79133b3fa0d9124b5fc83fa
Ervin <opilane@HP-CZC0359NN4>
2021-12-07 13:29:31 +0200

    Add project files.

---
+++ ExercisesPart1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExercisesPart1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, lastname, year;
+            int age;
+            int currentYear = DateTime.Now.Year;
+            Console.WriteLine("Print name");
+            name = Console.ReadLine();
+            Console.WriteLine("Print lastname");
+            lastname = Console.ReadLine();
+            Console.WriteLine($"Hello {name} {lastname}");
+            Console.WriteLine("The year you were born?");
+            year = Console.ReadLine();
+            age = currentYear - Convert.ToInt32(year);
+            if (age>=13)
+            {
+                Console.WriteLine("Welcome to Instagram");
+            }
+            else
+            {
+                Console.WriteLine("You are too young to have an Instagram account");
+            }
+        }
+    }
+}
---
+++ PersonalityTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PersonalityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userColor;
+            Console.WriteLine("Print your faviurite color please!");
+            userColor = Console.ReadLine().ToLower();
+            if (userColor == "red")
+            {
+                Console.WriteLine("You are romantic");
+            }
+            else if (userColor == "blue")
+            {
+                Console.WriteLine("You are cheerful");
+            }
+            else if (userColor == "reliable")
+            {
+                Console.WriteLine("You are cheerful");
+            }
+            else
+            {
+                Console.WriteLine($"You are {userColor} Unicorn");
+            }
+        }
+    }
+}
